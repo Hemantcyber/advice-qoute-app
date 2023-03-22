61663b2968bef069b5dd5a3f1aeed20b55dd817c
hemantjatav <hemanthikke@gmail.com>
2023-03-22 13:59:18 +0530

    initial commit

---
+++ src/Qoutegenerator/Qoute.js
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+import "./Qoute.css";
+
+class Qoute extends React.Component {
+  state = { advie: "" };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="main">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Qoute;
